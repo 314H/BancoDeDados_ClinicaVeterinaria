bfdf4e47d198fa833cd7122d15525154f8353787
Thaís Fernandes <thais.fernandes.001@acad.pucrs.br>
2019-05-24 17:22:36 -0300

    exames populados

--- PopulandoExames.sql
+++ PopulandoExames.sql
@@ -1,2 +1,35 @@
 INSERT INTO EXAMES (cod_atendimento, cod_exame, valor_praticado)
-VALUES (); 
+VALUES (11000,119,90.90); 
+
+INSERT INTO EXAMES (cod_atendimento, cod_exame, valor_praticado)
+VALUES (12000,111,235.20); 
+
+INSERT INTO EXAMES (cod_atendimento, cod_exame, valor_praticado)
+VALUES (12000,115,80.00); 
+
+INSERT INTO EXAMES (cod_atendimento, cod_exame, valor_praticado)
+VALUES (14000,118,435.30); 
+
+INSERT INTO EXAMES (cod_atendimento, cod_exame, valor_praticado)
+VALUES (15000,117,184.40); 
+
+INSERT INTO EXAMES (cod_atendimento, cod_exame, valor_praticado)
+VALUES (17000,113,105.00); 
+
+INSERT INTO EXAMES (cod_atendimento, cod_exame, valor_praticado)
+VALUES (18000,112,125.93); 
+
+INSERT INTO EXAMES (cod_atendimento, cod_exame, valor_praticado)
+VALUES (19000,118,355.00); 
+
+INSERT INTO EXAMES (cod_atendimento, cod_exame, valor_praticado)
+VALUES (20000,115,80.00); 
+
+INSERT INTO EXAMES (cod_atendimento, cod_exame, valor_praticado)
+VALUES (20000,117,184.40); 
+
+INSERT INTO EXAMES (cod_atendimento, cod_exame, valor_praticado)
+VALUES (20000,119,90.00); 
+
+INSERT INTO EXAMES (cod_atendimento, cod_exame, valor_praticado)
+VALUES (22000,114,145.00); 
