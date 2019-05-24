6523682898d01229af06c3f87acbb71d1a0e6852
Thaís Fernandes <thais.fernandes.001@acad.pucrs.br>
2019-05-24 17:21:16 -0300

    atendimentos populados

--- PopulandoAtendimentos.sql
+++ PopulandoAtendimentos.sql
@@ -1,2 +1,38 @@
 INSERT INTO ATENDIMENTOS (cod_atendimento, dataHora, diagnostico, crv_atendente, cod_paciente, exame)
-VALUES ();
+VALUES (10000,DATE '2019-01-19', 'Saudável',110,1901,0);
+
+INSERT INTO ATENDIMENTOS (cod_atendimento, dataHora, diagnostico, crv_atendente, cod_paciente, exame)
+VALUES (11000,DATE '2019-01-25', 'Cegueira',115,1902,1);
+
+INSERT INTO ATENDIMENTOS (cod_atendimento, dataHora, diagnostico, crv_atendente, cod_paciente, exame)
+VALUES (12000,DATE '2019-02-05', 'Diabetes',120,1903,2);
+
+INSERT INTO ATENDIMENTOS (cod_atendimento, dataHora, diagnostico, crv_atendente, cod_paciente, exame)
+VALUES (13000,DATE '2019-02-14', 'Saudável',125,1904,0);
+
+INSERT INTO ATENDIMENTOS (cod_atendimento, dataHora, diagnostico, crv_atendente, cod_paciente, exame)
+VALUES (14000,DATE '2019-02-22', 'FIV',130,1905,1);
+
+INSERT INTO ATENDIMENTOS (cod_atendimento, dataHora, diagnostico, crv_atendente, cod_paciente, exame)
+VALUES (15000,DATE '2019-03-01', 'Arritmia cardíaca',135,1906,1);
+
+INSERT INTO ATENDIMENTOS (cod_atendimento, dataHora, diagnostico, crv_atendente, cod_paciente, exame)
+VALUES (16000,DATE '2019-03-03', 'Saudável',140,1907,0);
+
+INSERT INTO ATENDIMENTOS (cod_atendimento, dataHora, diagnostico, crv_atendente, cod_paciente, exame)
+VALUES (17000,DATE '2019-03-12', 'Infecção renal',145,1908,1);
+
+INSERT INTO ATENDIMENTOS (cod_atendimento, dataHora, diagnostico, crv_atendente, cod_paciente, exame)
+VALUES (18000,DATE '2019-03-24', 'Vermes',150,1910,1);
+
+INSERT INTO ATENDIMENTOS (cod_atendimento, dataHora, diagnostico, crv_atendente, cod_paciente, exame)
+VALUES (19000,DATE '2019-03-30', 'FeLV',155,1911,1);
+
+INSERT INTO ATENDIMENTOS (cod_atendimento, dataHora, diagnostico, crv_atendente, cod_paciente, exame)
+VALUES (20000,DATE '2019-04-11', 'Diabetes',110,1912,3);
+
+INSERT INTO ATENDIMENTOS (cod_atendimento, dataHora, diagnostico, crv_atendente, cod_paciente, exame)
+VALUES (21000,DATE '2019-04-20', 'Saudável',115,1913,0);
+
+INSERT INTO ATENDIMENTOS (cod_atendimento, dataHora, diagnostico, crv_atendente, cod_paciente, exame)
+VALUES (22000,DATE '2019-05-23', 'Hepatite',120,1914,1);
